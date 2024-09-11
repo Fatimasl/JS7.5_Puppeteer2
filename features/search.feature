c56d056a3cf268ae30a2cc6ed3f1cdd422ce9c7f
Neto test <s.e.lebedeva74@gmail.com>
2024-09-11 08:14:08 +0500

    cucumber tests added

--- features/search.feature
+++ features/search.feature
@@ -1,5 +1,35 @@
-Feature: Search a course
+Feature: Movie tests
     Scenario: Should search by text
         Given user is on "/navigation" page
         When user search by "тестировщик"
-        Then user sees the course suggested "Тестировщик ПО"
+        Then user sees the course suggested "программист"
+
+    
+    Scenario: Should book the first available seat
+        Given user is on go_to_the_movie page
+        When user chooses the day "2"
+        When user chooses first abled seances_time
+        When user chooses first abled "standart" seat
+        When user clicks book button
+        Then user sees notice "Вы выбрали билеты:"
+    
+    Scenario: Should book the two seats for specific movie
+        Given user is on go_to_the_movie page
+        When user chooses the day "3"
+        When user chooses specific seances_time "190"
+        When user chooses on "5" row "5" seat
+        When user chooses on "5" row "6" seat
+        When user clicks book button
+        Then user sees notice "Вы выбрали билеты:"
+    
+    Scenario: First sad path test
+        Given user is on go_to_the_movie page
+        When user chooses the day "4"
+        When user chooses specific seances_time "198"
+        When user chooses first disabled seat
+        Then book button "Забронировать" is unclickable
+    
+    Scenario: Second sad path test
+        Given user is on go_to_the_movie page
+        When user chooses disabled seances_time
+        Then user is on the same page and sees seans to the movie "Сталкер"  
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -1,9 +1,11 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { Given, When, Then, Before, After, setDefaultTimeout } = require("cucumber");
+const { clickElement, getText, putText } = require("../../lib/commands.js");
 
+//setDefaultTimeout(10000);
+setDefaultTimeout(10000);
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
   const page = await browser.newPage();
@@ -32,3 +34,88 @@ Then("user sees the course suggested {string}", async function (string) {
   const expected = await string;
   expect(actual).contains(expected);
 });
+
+Given("user is on go_to_the_movie page", async function () {
+  return await this.page.goto(`https://qamid.tmweb.ru/client/index.php`, {
+    setTimeout: 20000,
+  });
+});
+
+When("user chooses the day {string}", async function (string) {
+  return await clickElement(this.page, `nav > a:nth-child(${string})`);
+});
+
+When("user chooses first abled seances_time", async function () {
+  return await clickElement(
+    this.page,
+    ".movie-seances__time:not(.acceptin-button-disabled)"
+  ); 
+});
+
+When("user chooses specific seances_time {string}", async function (string) {
+  return await clickElement(
+    this.page,
+    `.movie-seances__time[href='#'][data-seance-id='${string}']`
+  );
+});
+
+When("user chooses disabled seances_time", async function () {
+  const selector = ".movie-seances__time.acceptin-button-disabled";
+  
+  const buttonsToPress = await this.page.$$(selector);
+  
+  if (buttonsToPress.length > 0) {
+    return await buttonsToPress[0].clickElement;
+  } 
+    return;
+});
+
+When("user chooses first abled {string} seat", async function (string) {
+  return await clickElement(
+    this.page,
+    `.buying-scheme__chair.buying-scheme__chair_${string}:not(buying-scheme__chair_taken)`
+  );
+});
+
+When(
+  "user chooses on {string} row {string} seat",  async function (string, string2) {
+    return await clickElement(
+      this.page,
+      `section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${string}) > span:nth-child(${string2})`
+    );
+  }
+);
+When("user chooses first disabled seat", async function () {
+  return await clickElement(
+    this.page,
+    `.buying-scheme__chair.buying-scheme__chair_disabled`
+  );
+});
+
+When("user clicks book button", async function () {
+  return await clickElement(this.page, ".acceptin-button");
+});
+
+Then("user sees notice {string}", async function (string) {
+  const actual = await getText(this.page, ".ticket__check-title");
+  const expected = await string;
+  expect(actual).contains(expected);
+});
+
+Then("book button {string} is unclickable", async function (string) {
+  const actual = await getText(this.page, ".acceptin-button[disabled='true']");
+  const expected = await string;
+  expect(actual).contains(expected);
+});
+
+Then(
+  "user is on the same page and sees seans to the movie {string}",
+  async function (string) {
+    const actual = await getText(
+      this.page,
+      "body main section:nth-child(1) div:nth-child(1) div:nth-child(2) h2:nth-child(1)"
+    );
+    const expected = await string;
+    expect(actual).contains(expected);
+  }
+);
